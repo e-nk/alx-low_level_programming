d10b33563e43aa95234b48d121ef1cabc877bf5b
e-nk <enocklagat747@gmail.com>
2022-06-07 11:04:36 -0700

    task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ *  * main -  prints Holberton, followed by a new line
+ *   * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char *my_string = "_putchar";
+
+	while (*my_string != '\0')
+	{
+		_putchar (*my_string);
+		my_string++;
+	}
+
+	_putchar ('\n');
+
+	return (0);
+}
